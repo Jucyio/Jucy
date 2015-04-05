43fd422599972f9385c9f3f9bc5a9a2e5947e0ea
Uriel Corfa <uriel@corfa.fr>
2015-04-04 21:35:22 -0700

    Check HMAC digests in webhook notifications before handling them.
    Bump #1

--- web/webhooks.py
+++ web/webhooks.py
@@ -1,6 +1,8 @@
+import hashlib
+import hmac
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseNotFound
-
+from web import jucybot
 
 def handle_ping(request, repo):
     return HttpResponse()
@@ -14,10 +16,21 @@ def handle_issue_comment(request, repo):
     return HttpResponse()
 
 
+def verify_signature(request, repo):
+    signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    if not signature:
+        return False
+    repo_secret = jucybot.getSecretForRepo(repo)
+    h = hmac.new(repo_secret, request.body, digestmod=hashlib.sha1)
+    reference = 'sha1=%s' % h.hexdigest()
+    return reference == signature
+
 def dispatch(request, repo, hook):
     github_event = request.META.get('HTTP_X_GITHUB_EVENT')
     if not github_event:
         return HttpResponseNotFound('No X-GitHub-Event!')
+    if not verify_signature(request, repo):
+        return HttpResponseNotFound('X-Hub-Signature did not verify')
     if github_event == 'ping':
         return handle_ping(request, repo)
     elif github_event == 'issue_comment':
