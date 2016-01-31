a187985cf076f889e5eb348367099d30dfd3d7f4
db0company <db0company@gmail.com>
2016-01-31 21:56:43 +0100

    Fix invalid import

--- widget/urls.py
+++ widget/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from widget import views
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='widget_test.html'), name='widget_test'),
